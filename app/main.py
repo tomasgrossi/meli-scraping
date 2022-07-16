03dee44903bac2dad62ca40492a4984785d03e8c
Tomas Grossi <tomasgrossi@edmachina.com>
2022-07-16 18:22:55 -0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from app.src.utils.settings import Settings
+from fastapi.middleware.cors import CORSMiddleware
 from .src.controllers import Router
 
 settings = Settings()
 
 app = FastAPI()
 
+
+# Cors
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(Router)
-
-
-
